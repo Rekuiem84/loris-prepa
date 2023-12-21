df8caceb2e91ccb156367522f0c72f8ae85795b2
Rekuiem84 <morinijoshua@gmail.com>
2023-12-21 22:14:07 +0100

    ajout burger menu pour responsive

--- script.js
+++ script.js
@@ -1,25 +1,7 @@
-// const scrollers = document.querySelectorAll(".scroller");
+const Burger = document.querySelector(".burger-cont");
+const NavMenu = document.querySelector(".nav__list");
 
-// // If a user hasn't opted in for recuded motion, then we add the animation
-// if (!window.matchMedia("(prefers-reduced-motion: reduce)").matches) {
-// 	addAnimation();
-// }
-
-// function addAnimation() {
-// 	scrollers.forEach((scroller) => {
-// 		// add data-animated="true" to every `.scroller` on the page
-// 		scroller.setAttribute("data-animated", true);
-
-// 		// Make an array from the elements within `.scroller-inner`
-// 		const scrollerInner = scroller.querySelector(".scroller__inner");
-// 		const scrollerContent = Array.from(scrollerInner.children);
-
-// 		{
-// 			scrollerContent.forEach((item) => {
-// 				const duplicatedItem = item.cloneNode(true);
-// 				duplicatedItem.setAttribute("aria-hidden", true);
-// 				scrollerInner.appendChild(duplicatedItem);
-// 			});
-// 		}
-// 	});
-// }
+Burger.addEventListener("click", () => {
+	Burger.classList.toggle("open");
+	NavMenu.classList.toggle("open");
+});
