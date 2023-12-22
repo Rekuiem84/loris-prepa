5586fc232e53813c7a03f9aa080548dd6b56cbb6
Rekuiem84 <morinijoshua@gmail.com>
2023-12-22 16:47:37 +0100

    ajout du captcha et ajoute la préférence reduced-motion

--- script.js
+++ script.js
@@ -5,3 +5,16 @@ Burger.addEventListener("click", () => {
 	Burger.classList.toggle("open");
 	NavMenu.classList.toggle("open");
 });
+
+// function validateForm() {
+// 	var response = grecaptcha.getResponse();
+
+// 	if (response.length === 0) {
+// 		// Le reCAPTCHA n'est pas validé, empêchez la soumission du formulaire
+// 		alert("Veuillez cocher la case 'Je ne suis pas un robot'");
+// 		return false;
+// 	}
+
+// 	// Le reCAPTCHA est validé, autorisez la soumission du formulaire
+// 	return true;
+// }
